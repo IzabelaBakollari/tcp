551e6e5ae0e877f0d9e1f41126008bb13537b530
Izabela bakollari <izabela.bakollari@gmail.com>
2021-11-16 10:04:55 +0100

    Create an infinte loop to print "y"

--- yes.c
+++ yes.c
@@ -9,42 +9,47 @@
 #include <liburing.h>
 #include <limits.h>
 
+#define BUF_SZ 4096
+
 int fixed_buffers(struct io_uring *ring) {
 
 	struct iovec iov;
 	struct io_uring_sqe *sqe;
 	struct io_uring_cqe *cqe;
 	int i;
-	int size = 4096;
-	char buf[size];
+	char buf[BUF_SZ];
+
 
 	iov.iov_base = buf;
-	iov.iov_len = size;
-
-	for (i = 0; i<=size; i++) {
+	iov.iov_len = BUF_SZ;
+	
+	for (i = 0; i<BUF_SZ; i++) {
 		if (i%2 == 0)
 			buf[i] = 'y';
 		else
 			buf[i] = '\n';
 	}
-
+	
 	int ret = io_uring_register_buffers(ring, &iov, 1);
 	
 	if (ret) {
 		fprintf(stderr, "Error registering buffers: %s\n", strerror(-ret));
 		return 1;
 	}
-	sqe = io_uring_get_sqe(ring);
-
-	if (!sqe) {
-		fprintf(stderr, "Could not get SQE.\n");
-		return 1;
-	}
-	
-	io_uring_prep_write_fixed(sqe, 1, iov.iov_base, size, 0, 0);
 
 	for (;;) {
+		
+		sqe = io_uring_get_sqe(ring);
+
+		if (!sqe) {
+			fprintf(stderr, "Could not get SQE.\n");
+			return 1;
+		}
+	
+		io_uring_prep_write_fixed(sqe, 1, iov.iov_base, BUF_SZ, 0, 0);
+	
 		io_uring_submit(ring);
+
 	}
 
 	ret = io_uring_wait_cqe(ring, &cqe);
