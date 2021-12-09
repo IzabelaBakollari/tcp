45e1dba2770d2e29c474d4166b133f1ea13de71c
Izabela bakollari <izabela.bakollari@gmail.com>
2021-12-09 15:49:52 +0100

    Socket connection using io_uring

--- server.c
+++ server.c
@@ -1,186 +1,139 @@
-//Example code: A simple server side code, which echos back the received message.
-//Handle multiple socket connections with select and fd_set on Linux
+#include <fcntl.h>
 #include <stdio.h>
-#include <string.h> //strlen
-#include <stdlib.h>
-#include <errno.h>
-#include <unistd.h> //close
-#include <arpa/inet.h> //close
-#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/io_uring.h>
+#include <stdlib.h>
+#include <liburing.h>
+#include <unistd.h>
 #include <netinet/in.h>
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
-	
-#define TRUE 1
-#define FALSE 0
-#define PORT 8888
-	
-int main(int argc , char *argv[])
+#include <netinet/tcp.h>
+
+#define PORT   8080
+#define BUF_SZ 1024
+
+int main(int argc, char const *argv[])
 {
-	int opt = TRUE;
-	int master_socket , addrlen , new_socket , client_socket[30] ,
-		max_clients = 30 , activity, i , valread , sd;
-	int max_sd;
+	int server_fd, new_socket;
+	//int valread;
 	struct sockaddr_in address;
-		
-	char buffer[1025]; //data buffer of 1K
-		
-	//set of socket descriptors
-	fd_set readfds;
-		
-	//a message
-	char *message = "ECHO Daemon v1.0 \r\n";
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buf[BUF_SZ] = {0};
+	char *hello = "Hello from server";
+	struct io_uring ring;
+	struct io_uring_sqe *sqe;
+	struct io_uring_cqe *cqe;
+
 	
-	//initialise all client_socket[] to 0 so not checked
-	for (i = 0; i < max_clients; i++)
-	{
-		client_socket[i] = 0;
-	}
-		
-	//create a master socket
-	if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 	{
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
 	
-	//set master socket to allow multiple connections ,
-	//this is just a good habit, it will work without this
-	if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
-		sizeof(opt)) < 0 )
+	// Forcefully attaching socket to the port 8080
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+			&opt, sizeof(opt)))
 	{
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
-	
-	//type of socket created
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
-		
-	//bind the socket to localhost port 8888
-	if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
+	address.sin_port = htons(PORT);
+	
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr *)&address,
+		sizeof(address))<0)
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-	printf("Listener on port %d \n", PORT);
-		
-	//try to specify maximum of 3 pending connections for the master socket
-	if (listen(master_socket, 3) < 0)
+
+	if (listen(server_fd, 3) < 0)
 	{
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
-		
-	//accept the incoming connection
-	addrlen = sizeof(address);
-	puts("Waiting for connections ...");
-		
-	while(TRUE)
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+					(socklen_t*)&addrlen))<0)
 	{
-		//clear the socket set
-		FD_ZERO(&readfds);
-	
-		//add master socket to set
-		FD_SET(master_socket, &readfds);
-		max_sd = master_socket;
-			
-		//add child sockets to set
-		for ( i = 0 ; i < max_clients ; i++)
-		{
-			//socket descriptor
-			sd = client_socket[i];
-				
-			//if valid socket descriptor then add to read list
-			if(sd > 0)
-				FD_SET( sd , &readfds);
-				
-			//highest file descriptor number, need it for the select function
-			if(sd > max_sd)
-				max_sd = sd;
-		}
-	
-		//wait for an activity on one of the sockets , timeout is NULL ,
-		//so wait indefinitely
-		activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
-	
-		if ((activity < 0) && (errno!=EINTR))
-		{
-			printf("select error");
-		}
-			
-		//If something happened on the master socket ,
-		//then its an incoming connection
-		if (FD_ISSET(master_socket, &readfds))
-		{
-			if ((new_socket = accept(master_socket,
-					(struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-			{
-				perror("accept");
-				exit(EXIT_FAILURE);
-			}
-			
-			//inform user of socket number - used in send and receive commands
-			printf("New connection , socket fd is %d , ip is : %s , port : %d\n",
-				new_socket , inet_ntoa(address.sin_addr) , ntohs
-				(address.sin_port));
-		
-			//send new connection greeting message
-			if( send(new_socket, message, strlen(message), 0) != strlen(message) )
-			{
-				perror("send");
-			}
-				
-			puts("Welcome message sent successfully");
-				
-			//add new socket to array of sockets
-			for (i = 0; i < max_clients; i++)
-			{
-				//if position is empty
-				if( client_socket[i] == 0 )
-				{
-					client_socket[i] = new_socket;
-					printf("Adding to list of sockets as %d\n" , i);
-						
-					break;
-				}
-			}
-		}
-			
-		//else its some IO operation on some other socket
-		for (i = 0; i < max_clients; i++)
-		{
-			sd = client_socket[i];
-				
-			if (FD_ISSET( sd , &readfds))
-			{
-				//Check if it was for closing , and also read the
-				//incoming message
-				if ((valread = read( sd , buffer, 1024)) == 0)
-				{
-					//Somebody disconnected , get his details and print
-					getpeername(sd , (struct sockaddr*)&address , \
-						(socklen_t*)&addrlen);
-					printf("Host disconnected , ip %s , port %d \n" ,
-						inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
-						
-					//Close the socket and mark as 0 in list for reuse
-					close( sd );
-					client_socket[i] = 0;
-				}
-					
-				//Echo back the message that came in
-				else
-				{
-					//set the string terminating NULL byte on the end
-					//of the data read
-					buffer[valread] = '\0';
-					send(sd , buffer , strlen(buffer) , 0 );
-				}
-			}
-		}
+		perror("accept");
+		exit(EXIT_FAILURE);
 	}
-		
+
+	//valread = read( new_socket , buf, BUF_SZ);
+	//printf("%s\n",buf);
+	//send(new_socket , hello , strlen(hello) , 0 );
+	//printf("Hello message sent\n");
+
+	// Start using io_uring library
+
+	int ret = io_uring_queue_init(8, &ring, 0);
+
+	if (ret) {
+		fprintf(stderr, "Unable to setup io_uring: %s\n", strerror(-ret));
+		return 1;
+	}
+
+	sqe = io_uring_get_sqe(&ring);
+
+	if (!sqe) {
+		fprintf(stderr, "Could not get SQE.\n");
+		return 1;
+	}
+
+	//void io_uring_prep_read(struct io_uring_sqe *sqe, int fd, void *buf, unsigned nbytes, off_t offset)
+
+	io_uring_prep_read(sqe, new_socket, buf, BUF_SZ, 0);
+	sqe->flags |= IOSQE_IO_LINK;
+
+	printf("%s\n",buf);
+
+
+	sqe = io_uring_get_sqe(&ring);
+
+	if (!sqe) {
+		fprintf(stderr, "Could not get SQE.\n");
+		return 1;
+	}
+
+	//void io_uring_prep_write(struct io_uring_sqe *sqe, int fd, const void *buf, unsigned nbytes, off_t offset)
+	
+	io_uring_prep_write(sqe, new_socket, hello, BUF_SZ, 0);
+	sqe->flags |= IOSQE_IO_LINK;
+
+	printf("Hello message sent\n");
+
+
+	ret = io_uring_submit_and_wait(&ring, 2);
+
+	if (ret < 0) {
+		fprintf(stderr, "Error submitting buffers: %s\n", strerror(-ret));
+		return 1;
+	}
+
+	for (int i = 0; i < 2; i++) { 
+
+		ret = io_uring_peek_cqe(&ring, &cqe);
+	
+		if (ret < 0) {
+			fprintf(stderr, "Error waiting for completion: %s\n", strerror(-ret));
+			return 1;
+		}
+
+		if (cqe->res < 0) {
+			fprintf(stderr, "Error in async operation: %s\n", strerror(-cqe->res));
+		}
+
+		io_uring_cqe_seen(&ring, cqe);
+	}
+
+	io_uring_queue_exit(&ring);
+
 	return 0;
 }
